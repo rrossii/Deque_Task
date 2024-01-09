b75af5335abac9799e4b48f8e91e9f8e9054d106
rrossii <annros2003@gmail.com>
2024-01-09 14:43:13 +0200

    done

--- src/main/java/codefinity/Main.java
+++ src/main/java/codefinity/Main.java
@@ -1,7 +1,32 @@
 package codefinity;
 
+import codefinity.Task.BrowserHistory;
+import codefinity.Task.BrowserHistoryImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("You can test your solution here!");
+        BrowserHistory browser = new BrowserHistoryImpl();
+
+        System.out.println("Default page: " + browser.getCurrentPage());
+
+        browser.visitPage("https://codefinity.com/url1");
+        browser.visitPage("https://codefinity.com/url2");
+        browser.visitPage("https://codefinity.com/url3");
+
+        System.out.println("Current Page after visiting 3 pages: " + browser.getCurrentPage());
+
+        browser.goBack();
+        browser.goBack();
+        browser.goBack();
+
+        System.out.println("Current Page after going back 2 times: " + browser.getCurrentPage());
+
+        browser.goForward();
+        browser.goForward();
+        browser.goForward();
+        browser.goForward();
+
+        System.out.println("Current Page after going forward: " + browser.getCurrentPage());
+
     }
 }
--- src/main/java/codefinity/Task/BrowserHistoryImpl.java
+++ src/main/java/codefinity/Task/BrowserHistoryImpl.java
@@ -1,4 +1,55 @@
 package codefinity.Task;
 
-public class BrowserHistoryImpl {
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class BrowserHistoryImpl implements BrowserHistory {
+    private Deque<String> backStack, forwardStack;
+    private String currentUrl;
+    public BrowserHistoryImpl(Deque<String> backStack, Deque<String> forwardStack, String initialUrl) {
+        this.backStack = backStack;
+        this.forwardStack = forwardStack;
+        this.currentUrl = initialUrl;
+    }
+    public BrowserHistoryImpl() {
+        backStack = new LinkedList<>();
+        forwardStack = new LinkedList<>();
+        currentUrl = "";
+    }
+    @Override
+    public void visitPage(String url) {
+        backStack.push(currentUrl);
+        forwardStack.clear();
+
+        currentUrl = url;
+        System.out.println("Currently at: " + currentUrl);
+
+    }
+
+    @Override
+    public void goBack() {
+        if (!backStack.isEmpty()) {
+            forwardStack.push(currentUrl);
+            currentUrl = backStack.pop();
+            System.out.println("Go back: " + currentUrl);
+        } else {
+            System.out.println("Cannot go back. Already at the beginning.");
+        }
+    }
+
+    @Override
+    public void goForward() {
+        if (!forwardStack.isEmpty()) {
+            backStack.push(currentUrl);
+            currentUrl = forwardStack.pop();
+            System.out.println("Go forward: " + currentUrl);
+        } else {
+            System.out.println("Cannot go forward. Already at the latest page.");
+        }
+    }
+
+    @Override
+    public String getCurrentPage() {
+        return currentUrl;
+    }
 }
